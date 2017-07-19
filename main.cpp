aea972dcb2e089793331ca716656b595c5d7e88d
Derek Fulton <derekfulton@dhcp-172-24-142-218.eduroam.wireless.private.cam.ac.uk>
2017-07-19 13:27:06 +0100

    Identify low hanging fruit

--- main.cpp
+++ main.cpp
@@ -4,141 +4,209 @@
 #include "opencv2/highgui.hpp"
 #include <stdio.h>
 #include <iostream>
+#include <string>
 using namespace cv;
 using namespace std;
 // http://docs.opencv.org/2.4/doc/tutorials/imgproc/shapedescriptors/bounding_rotated_ellipses/bounding_rotated_ellipses.html
 // https://cci.lbl.gov/cctbx_sources/spotfinder/core_toolbox/hough.cpp
 
 Mat src_gray;
-int thresh = 64; // Starting threshold
-int max_thresh = 255;
+int thresh = 4; // Starting threshold
+int max_thresh = 100;
 RNG rng(12345);
 
-// Funciton header
+// Function declarations
 void thresh_callback(int, void* );
+vector<int> classifyEllipses(vector<RotatedRect> ellipses, vector<int> neighbor_counts);
+bool hasNeighbor(RotatedRect ellipse, vector<RotatedRect> ellipses);
+
 
 int main( int argc, const char** argv )
 {
-    String filename = "/Users/derekfulton/Documents/yeast/cv_yeast/cv_yeast/transes/T7.tif";
-    // Print message
-    std::cout << "Displaying: " << filename << endl;
-    
-    // Read the image in grayscale
+//    String filename = "/Users/derekfulton/Documents/yeast/cv_yeast/cv_yeast/transes/T10.tif";
+    String filename = argv[1];
     src_gray = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
-    //    cvtColor( src_gray, src_gray, CV_BGR2GRAY );
-    blur( src_gray, src_gray, Size(1,1) );
+
+    blur( src_gray, src_gray, Size(1,1) ); // Gaussian blur
+    
+    String source_window = "Source";
+    
+//    namedWindow( source_window, CV_WINDOW_AUTOSIZE );
+//    
+//    imshow( source_window, src_gray);
+    
+    Mat canny_output;
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    
+    Canny( src_gray, canny_output, thresh, thresh*8, 3 ); // Edges
+    
+    findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0)); // Contours
+    
+    vector<RotatedRect> minRect ( contours.size() ); // Vector of RotatedRects called minRect
+    vector<RotatedRect> minEllipse ( contours.size() ); // Vector of RotatedRects called minEllipse
     
     
-    // Make window for the source
-    char* source_window = "Source";
-    namedWindow( source_window, CV_WINDOW_AUTOSIZE );
+    // Make one ellipse per contour, not vetted yet
+    for( int i = 0; i < contours.size(); i++ ) {
+        if ( contours[i].size() < 5 ) {
+            // Not big enough to fit the ellipse
+        }
+        else {
+            RotatedRect prospectiveEllipse = fitEllipse(Mat(contours[i]));
+            // Features of each fitted ellipse
+            float major = prospectiveEllipse.size.width;
+            float minor = prospectiveEllipse.size.height;
+            float area = prospectiveEllipse.size.area();
+            float aspect_ratio = major / minor;
+            // Validate
+            
+            float min_area = 200;
+            float max_area = 30000.00;
+            
+            if(aspect_ratio < 0.4 || area < 2000 || prospectiveEllipse.size.height > 130){
+                // Doesn't make the cut
+            }
+            else {
+                minRect[i] = minAreaRect( Mat(contours[i]) );
+                minEllipse[i] = fitEllipse( Mat(contours[i]) );
+            }
+        }
+    }
     
-    // Show it
-    imshow( source_window, src_gray);
+    Mat overlay = imread(argv[1], CV_LOAD_IMAGE_COLOR);
     
-    // Make slider
-    createTrackbar( " Threshold:", source_window, &thresh, max_thresh, thresh_callback);
-    thresh_callback(0, 0);
     
+    vector<int> initial_counts(minEllipse.size());
+    
+    // Make intial counts 0 because we start with zero neighbors
+    for (int i = 0; i < minEllipse.size(); i++) {
+        initial_counts[i] = 0;
+    }
+    
+    vector<int> neighbor_counts = classifyEllipses(minEllipse, initial_counts);
+    Scalar color;
+    
+    for( int i = 0; i < minEllipse.size(); i++) {
+        
+        
+        
+        
+        //            // Contour
+        //            drawContours(canny_drawing, contours, i, color, 1, 8, vector<Vec4i>(), 0, Point());
+        //
+        // Ellipse
+        if (neighbor_counts[i] == 0) {
+            color = Scalar(1, 0, 0); // blue
+        }
+        else {
+            color = Scalar(0, 255, 0); // green
+            ellipse(overlay, minEllipse[i], color, 2, 8);
+        }
+        
+        //        std::string label = std::to_string(i);
+        //
+        //        putText(overlay, label, cvPoint(minEllipse[i].center.x, minEllipse[i].center.y),
+        //                FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(200,200,250), 1, CV_AA);
+        
+        //            ellipse(overlay, minEllipse[i], color, 2, 8);
+        
+        
+        //            // Rotated rectangle
+        //            Point2f rect_points[4];
+        //            minRect[i].points(rect_points);
+        //        for (int j = 0; j < 4; j++) {
+        //                line(overlay, rect_points[j], rect_points[(j + 1) % 4], color, 1, 8);
+        //        }
+    }
+    
+    // Show everything
+    namedWindow( "Canny Contours", CV_WINDOW_FREERATIO );
+    imshow( "Canny Contours", overlay );
+    //    namedWindow( "Canny", CV_WINDOW_FREERATIO );
+    //    imshow( "Canny", canny_output);
+
     waitKey(0);
+    
     return(0);
 }
 
 /** @function thresh_callback */
 void thresh_callback(int, void* )
 {
-    // Declare variables thresh output, contours and hierarchy
-    Mat threshold_output; Mat threshold_intermediate; Mat canny_output; Mat og_output;
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
+    // Declare
+
     
-    std::cout << "thresh is: " << thresh << endl;
-    
-//    // Detect edges using threshold (try canny later?)
-//    threshold( src_gray, canny_output, thresh, 255, THRESH_BINARY);
-    
-        // Detect edges using canny BUT STILL CALL IT threshold_output
-        Canny( src_gray, canny_output, thresh, thresh*2, 3 );
-    
-    
-    // Find contours
-    findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-    
-    // FIND ELLIPSES
-    
-    // Find the rotated rectangles and ellipses for each contour (later try one or the other alone)
-    vector<RotatedRect> minRect ( contours.size() );
-    vector<RotatedRect> minEllipse ( contours.size() );
-    
-    // Make the ellipess
-    for( int i = 0; i < contours.size(); i++ )
-    {
-        minRect[i] = minAreaRect( Mat(contours[i]) );
-        std::cout << contours[i].size() << endl;
-        if( contours[i].size() > 5)
-        {
-            minEllipse[i] = fitEllipse( Mat(contours[i]) );
-        }
-    }
-    
-    // Draw contours and rotated rectangles and ellipses
-  
-    vector<vector<Point> >hull(contours.size());
-    vector<vector<int> > hullsI(contours.size()); // Indices to contour points
-    vector<vector<Vec4i>> defects(contours.size());
-    
-    Mat canny_drawing = imread("/Users/derekfulton/Documents/yeast/cv_yeast/cv_yeast/transes/T7.tif", CV_LOAD_IMAGE_COLOR);
-    for( int i = 0; i < contours.size(); i++)//contours.size(); i++ )
-    {
-        // ONLY DRAW IT IF IT MEETS THE RIGHT AREA
-        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-        
-        
-        // Make sure the ellipse is what we want
-        float major = minEllipse[i].size.width;
-        float minor = minEllipse[i].size.height;
-        float area = minEllipse[i].size.area();
-        float aspect_ratio = major / minor;
-        
-        convexHull(contours[i], hull[i], false);
-        convexHull(contours[i], hullsI[i], false);
-        if(hullsI[i].size() > 3 ) // You need more than 3 indices
-        {
-            convexityDefects(contours[i], hullsI[i], defects[i]);
-        }
-        
-        float min_area = 600;
-        float max_area = 15000.00;
-        
-        
-        if( area < min_area || area > max_area || aspect_ratio < 0.5) {
-            
-        }
-        else
-        {
-//            // Contour
-//            drawContours(canny_drawing, contours, i, color, 1, 8, vector<Vec4i>(), 0, Point());
-//            
-            // Ellipse
-            ellipse(canny_drawing, minEllipse[i], color, 2, 8);
-//
-            
-//            // Rotated rectangle
-//            Point2f rect_points[4];
-//            minRect[i].points(rect_points);
-//            for (int j = 0; j < 4; j++)
-//                line(canny_drawing, rect_points[j], rect_points[(j + 1) % 4], color, 1, 8);
-        }
-    }
-    
-    // Show in a window
-    namedWindow( "Canny Contours", CV_WINDOW_FREERATIO );
-    imshow( "Canny Contours", canny_drawing );
-    
-    // Show threshold
-    namedWindow( "Canny", CV_WINDOW_FREERATIO );
-    imshow( "Canny", canny_output);
 }
+
+vector<int> classifyEllipses(vector<RotatedRect> ellipses, vector<int> neighbor_counts) {
+    vector<Scalar> colors (ellipses.size());
+    // (BLUE, GREEN, RED)
+//    Scalar red = Scalar(0, 0, 255);
+//    Scalar green = Scalar(0, 255, 0);
+//    Scalar blue = Scalar(255, 0, 0);
+    for (int i = 0; i < ellipses.size(); i++) {
+        RotatedRect E1 = ellipses[i];
+        if (hasNeighbor(E1, ellipses)) { // If it meets the neighbor count criteria
+            neighbor_counts[i] = neighbor_counts[i] + 1;
+            continue;
+        }
+        else {
+            continue;
+        }
+    }
+    
+    return neighbor_counts;
+    
+}
+
+bool hasNeighbor(RotatedRect ellipse, vector<RotatedRect> ellipses) {
+    bool result = false;
+    float height = ellipse.size.height;
+    float width = ellipse.size.width;
+    Point2f center = ellipse.center;
+    float angle = ellipse.angle;
+    
+    for (int i = 0; i < ellipses.size(); i++) {
+        float curr_height = ellipses[i].size.height;
+        float curr_width = ellipses[i].size.width;
+        Point2f curr_center = ellipses[i].center;
+        float curr_angle = ellipses[i].angle;
+        float dist = sqrt( (ellipse.center.x - ellipses[i].center.x)*(ellipse.center.x - ellipses[i].center.x) + (ellipse.center.y - ellipses[i].center.y)*(ellipse.center.y - ellipses[i].center.y) );
+        float unweighted_difference_index = (1/4)*abs(height - curr_height) + (1/4)*abs(width - curr_width) + 10*dist + 10*abs(angle - curr_angle);
+        
+        
+        
+        if (ellipse.center == ellipses[i].center) { // If it's the same ellipse
+            continue;
+        }
+        else {
+            if ( unweighted_difference_index < 500) { // If it's 10 away and E1/E2 are relatively similar
+                result = true;
+                continue;
+            }
+            else {
+                continue;
+            }
+        }
+        
+        
+    }
+    return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
  THIS IS THE ITALIAN GUY'S ALGORITHM
  */
